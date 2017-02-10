ec147e429cd3ca7a0f058d152935d5004908eb27
Fjord82 <rfozz@hotmail.com>
2017-02-10 10:05:42 +0100

    First commit

---
+++ src/myattendance/DAL/FileManager.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myattendance.DAL;
+
+/**
+ *
+ * @author Fjord82
+ */
+public class FileManager
+{
+    
+}
---
+++ src/myattendance/MyAttendance.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myattendance;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Fjord82
+ */
+public class MyAttendance extends Application
+{
+    
+    @Override
+    public void start(Stage primaryStage)
+    {
+        
+        StackPane root = new StackPane();
+        
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+    
+}
