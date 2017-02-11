a68fdcd58962d55c0e16d3bfbf30d893ba589832
Fjord82 <rfozz@hotmail.com>
2017-02-11 09:06:25 +0100

    Merge branch 'Development' into RasmusDevelopment
    # Conflicts:
    #	src/myattendance/GUI/View/LoginView.fxml

--- src/myattendance/GUI/Model/AttendanceParser.java
+++ src/myattendance/GUI/Model/AttendanceParser.java
@@ -5,11 +5,62 @@
  */
 package myattendance.GUI.Model;
 
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import myattendance.MyAttendance;
+
 /**
  *
  * @author Fjord82
  */
 public class AttendanceParser
 {
-    
+
+    private static AttendanceParser instance;
+
+    public static AttendanceParser getInstance()
+    {
+
+        if (instance == null)
+        {
+            instance = new AttendanceParser();
+        }
+        return instance;
+    }
+
+    private AttendanceParser()
+    {
+
+    }
+
+    public Window stage;
+
+    /**
+     *
+     * @param title
+     * @param path
+     * @throws IOException
+     */
+    public void changeView(String title, String path) throws IOException
+    {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MyAttendance.class.getResource(path));
+        AnchorPane page = (AnchorPane) loader.load();
+
+        Stage dialogStage = new Stage();
+        dialogStage.initOwner(stage);
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+        Scene scene = new Scene(page);
+        dialogStage.setScene(scene);
+        dialogStage.setTitle(title);
+
+        dialogStage.show();
+    }
+
 }
