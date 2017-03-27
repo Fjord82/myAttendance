6ad1b51f18c3f803fe9dcb5aceb771ebd28d7dfd
JG-Ehmsen <jeppeehmsen@gmail.com>
2017-03-27 22:50:25 +0200

    Light cleaning

--- src/myattendance/GUI/Model/DateParser.java
+++ src/myattendance/GUI/Model/DateParser.java
@@ -11,11 +11,11 @@ import org.joda.time.DateTime;
  */
 public class DateParser
 {
-    
+
     BLLFacade bllFacade = BLLFacade.getInstance();
-    
+
     private static DateParser instance;
-    
+
     public static DateParser getInstance()
     {
         if (instance == null)
@@ -24,42 +24,37 @@ public class DateParser
         }
         return instance;
     }
-    
+
     private DateParser()
     {
-        
+
     }
-    
+
     public void getTodaysDate()
     {
         bllFacade.getTodaysDate();
     }
-    
-    public DateTime getLastLoginDate(User user)
-    {
-        return bllFacade.getLastLoginDate(user);
-    }
-    
+
     public int daysBetweenSpecificDateAndToday(DateTime specificDate)
     {
         return bllFacade.daysBetweenSpecificDateAndToday(specificDate);
     }
-    
+
     public DateTime getStartDate()
     {
         return bllFacade.getStartDate();
     }
-    
+
     public boolean isAbsent(User user, Day day)
     {
         return bllFacade.isAbsent(user, day);
     }
-    
+
     public Day getDay(DateTime dateTime)
     {
         return bllFacade.getDay(dateTime);
     }
-    
+
     public void recordAbsence(User user, Day today)
     {
         bllFacade.recordAbsence(user, today);
