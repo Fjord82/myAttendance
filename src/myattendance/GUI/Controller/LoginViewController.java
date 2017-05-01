c3f3b7f6017e68b65c5919676942932a5d3c6130
mngdnmw <mtdunmow@gmail.com>
2017-05-01 13:13:07 +0200

    Mockup user visible in UserInfoView

--- src/myattendance/GUI/Controller/LoginViewController.java
+++ src/myattendance/GUI/Controller/LoginViewController.java
@@ -7,7 +7,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
